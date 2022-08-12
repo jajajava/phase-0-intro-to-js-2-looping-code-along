0783291cc74ecd8cf5d54c37177b785627b00091
David <davidstepanov@yahoo.com>
2022-08-12 17:39:48 -0400

    commit tree c98ae554250f3f6a877ef9d1c6d9b9e29b3e5f4e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+  function giftWrapping(gifts){
+    for(let i=0; i< gifts.length ; i++)
+    console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    return gifts
+  }
+
+  giftWrapping(gifts)
+
+
+  const names = ["James","Michael","Susan"]
+  function thankYou(names){
+  for(let x= 0; x < names.length; x++)
+  console.log(`Thank you for the gift ${names[x]}!`)
+  return names
+  }
+  thankYou(names)
+
+  //Trying the while countDown
+
+
+function countDown(number){
+  let x= number
+while (x >= 0) {
+ 
+console.log(x)
+x--
+}
+}
+countDown(10)
+countDown(4)
